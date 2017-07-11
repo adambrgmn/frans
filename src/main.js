0356fba99f4b2918a0cc4b9b6881315945f35d2e
Adam Bergman <adam@fransvilhelm.com>
2017-07-11 15:20:17 +0200

    :sparkles: Add module reduceRight

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import _pipe from './pipe';
 import _compose from './compose';
 import _reduce from './reduce';
+import _reduceRight from './reduceRight';
 
 export const pipe = _pipe;
 export const compose = _compose;
 export const reduce = _reduce;
+export const reduceRight = _reduceRight;
