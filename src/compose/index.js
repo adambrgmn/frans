a65b5a19b6f1522c2eb546996e4df2cda9e7d1b1
Adam Bergman <adam@fransvilhelm.com>
2017-07-11 15:22:33 +0200

    :hammer: Implement reduceRight where possible

--- src/compose/index.js
+++ src/compose/index.js
@@ -1,4 +1,5 @@
 // @flow
+import reduceRight from '../reduceRight';
 
 export default (...fns: Array<GenericFn>) => (x: any) =>
-  fns.reduceRight((acc: any, fn: GenericFn) => fn(acc), x);
+  reduceRight((acc: any, fn: GenericFn) => fn(acc), x)(fns);
