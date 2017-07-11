56cce307cab7d1ab2ea78b45576c253d919dddb6
Adam Bergman <adam@fransvilhelm.com>
2017-07-11 15:44:31 +0200

    :sparkles: Add module map

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import _pipe from './pipe';
 import _compose from './compose';
 import _reduce from './reduce';
 import _reduceRight from './reduceRight';
+import _map from './map';
 
 export const pipe = _pipe;
 export const compose = _compose;
 export const reduce = _reduce;
 export const reduceRight = _reduceRight;
+export const map = _map;
