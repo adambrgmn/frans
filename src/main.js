e0e67af3aefef6eb6ccd9c9db0d9d1e271c26fb5
Adam Bergman <adam@fransvilhelm.com>
2017-07-12 11:43:14 +0200

    :twisted_rightwards_arrows: Add feature prop (#10)
    * :bug: Add // @flow-statement
    
    * :sparkles: Add module prop
    
    A module to get a prop from an object

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import _compose from './compose';
 import _reduce from './reduce';
 import _reduceRight from './reduceRight';
 import _map from './map';
+import _prop from './prop';
 
 export const pipe = _pipe;
 export const compose = _compose;
 export const reduce = _reduce;
 export const reduceRight = _reduceRight;
 export const map = _map;
+export const prop = _prop;
