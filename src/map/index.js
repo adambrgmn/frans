1990092025a9134d4035bf8b2499ed555ebff0f3
Adam Bergman <adam@fransvilhelm.com>
2017-07-12 14:26:41 +0200

    Revert ":twisted_rightwards_arrows: Add feature filter (#13)"
    This reverts commit aad23af5f1a69e0dd18bebf4ed85c928de803fbb.

--- src/map/index.js
+++
@@ -1,7 +0,0 @@
-// @flow
-
-export default (fn: GenericFn) => (arr: Array<any>) => {
-  const ret = [];
-  for (let i = 0; i < arr.length; i += 1) ret.push(fn(arr[i]));
-  return ret;
-};
--- src/reduceRight/index.js
+++
@@ -1,7 +0,0 @@
-// @flow
-
-export default (fn: GenericFn, init: any) => (arr: Array<any>) => {
-  let l = init;
-  for (let i = arr.length - 1; i >= 0; i -= 1) l = fn(l, arr[i]);
-  return l;
-};
