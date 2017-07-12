c0254b5f4d0dc52c6d745c841c58b6f6d2a19c28
Adam Bergman <adam@fransvilhelm.com>
2017-07-12 12:49:35 +0200

    :twisted_rightwards_arrows: Add feature reverse (#12)
    * :sparkles: Add module reverse
    
    Module reverse simply reverses a string or an array
    
    * :art: Add reverse as import/export of main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import _pipe from './pipe';
 import _prop from './prop';
 import _reduce from './reduce';
 import _reduceRight from './reduceRight';
+import _reverse from './reverse';
 
 export const compose = _compose;
 export const map = _map;
@@ -12,3 +13,4 @@ export const pipe = _pipe;
 export const prop = _prop;
 export const reduce = _reduce;
 export const reduceRight = _reduceRight;
+export const reverse = _reverse;
