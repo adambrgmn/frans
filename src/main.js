270e838fcb60634a3c5690e2538171189a838658
Adam Bergman <adam@fransvilhelm.com>
2017-07-12 14:49:23 +0200

    :twisted_rightwards_arrows: Add feature nth (#16)
    * :art: Add nth as import/export of main.js
    
    * :sparkles: Add feature nth
    
    Nth returns the nth element in an array or string

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 import _compose from './compose';
 import _filter from './filter';
 import _map from './map';
+import _nth from './nth';
 import _pipe from './pipe';
 import _prop from './prop';
 import _reduce from './reduce';
@@ -11,6 +12,7 @@ import _reverse from './reverse';
 export const compose = _compose;
 export const filter = _filter;
 export const map = _map;
+export const nth = _nth;
 export const pipe = _pipe;
 export const prop = _prop;
 export const reduce = _reduce;
