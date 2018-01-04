c296dda8d69624d04368d8c97b9ba7b3ddb834ac
Adam Bergman <adam@fransvilhelm.com>
2018-01-04 20:50:28 +0100

    fix(reverse): Move declarations inside fn scope

--- src/reverse.js
+++ src/reverse.js
@@ -2,10 +2,10 @@ import reduceRight from './reduceRight';
 import pipe from './pipe';
 import isArray from './isArray';
 
-const reverseArray = reduceRight((a, e) => a.concat(e), []);
-const reverseString = pipe(s => s.split(''), reverseArray, a => a.join(''));
-
 export default arr => {
+  const reverseArray = reduceRight((a, e) => a.concat(e), []);
+  const reverseString = pipe(s => s.split(''), reverseArray, a => a.join(''));
+
   if (isArray(arr)) return reverseArray(arr);
   return reverseString(arr);
 };
