24cb4779711df31222ae044eedbb3ee1c6042b35
Adam Bergman <adam@fransvilhelm.com>
2018-01-04 20:44:45 +0100

    fix(reverse): Use isArray-module

--- src/reverse.js
+++ src/reverse.js
@@ -1,10 +1,11 @@
 import reduceRight from './reduceRight';
 import pipe from './pipe';
+import isArray from './isArray';
 
 const reverseArray = reduceRight((a, e) => a.concat(e), []);
 const reverseString = pipe(s => s.split(''), reverseArray, a => a.join(''));
 
 export default arr => {
-  if (Array.isArray(arr)) return reverseArray(arr);
+  if (isArray(arr)) return reverseArray(arr);
   return reverseString(arr);
 };
