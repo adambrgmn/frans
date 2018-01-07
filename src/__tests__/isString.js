2aeb3617236c5944d36c4e28dd993babb8e99f49
Adam Bergman <adam@fransvilhelm.com>
2018-01-08 00:25:42 +0100

    feat(isString): Add mocule isString
    Checks if a value is a string

---
+++ src/__tests__/isString.js
@@ -0,0 +1,12 @@
+import isString from '../isString';
+
+test('Core.isString', () => {
+  expect(isString('a')).toBeTruthy();
+  expect(isString({})).toBeFalsy();
+  expect(isString({ foo: 'bar' })).toBeFalsy();
+  expect(isString(() => null)).toBeFalsy();
+  expect(isString([])).toBeFalsy();
+  expect(isString(1)).toBeFalsy();
+  expect(isString(null)).toBeFalsy();
+  expect(isString(undefined)).toBeFalsy();
+});
