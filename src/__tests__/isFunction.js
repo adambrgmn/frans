1ffd14dffd6be053637c76128240fd81247bbbb0
Adam Bergman <adam@fransvilhelm.com>
2018-01-08 00:22:40 +0100

    feat(isFunction): Add module isFunction
    Makes a typeof comparis to check if el is a function

---
+++ src/__tests__/isFunction.js
@@ -0,0 +1,12 @@
+import isFunction from '../isFunction';
+
+test('Core.isFunction', () => {
+  expect(isFunction(() => null)).toBeTruthy();
+  expect(isFunction({})).toBeFalsy();
+  expect(isFunction({ foo: 'bar' })).toBeFalsy();
+  expect(isFunction([])).toBeFalsy();
+  expect(isFunction('a')).toBeFalsy();
+  expect(isFunction(1)).toBeFalsy();
+  expect(isFunction(null)).toBeFalsy();
+  expect(isFunction(undefined)).toBeFalsy();
+});
