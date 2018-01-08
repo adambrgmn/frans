68d15c523da732d26f860de7fc23f5654767f83e
Adam Bergman <adam@fransvilhelm.com>
2018-01-08 17:23:54 +0100

    feat(keys): Add module keys
    Returns an array of keys of an object

---
+++ src/keys.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-underscore-dangle, no-restricted-syntax */
+import isObject from './isObject';
+import has from './has';
+import length from './length';
+import nth from './nth';
+
+export const _keys = obj => {
+  const hasDontEnumBug = !Object.prototype.propertyIsEnumerable.call(
+    { toString: null },
+    'toString',
+  );
+
+  const dontEnums = [
+    'toString',
+    'toLocaleString',
+    'valueOf',
+    'hasOwnProperty',
+    'isPrototypeOf',
+    'propertyIsEnumerable',
+    'constructor',
+  ];
+
+  const dontEnumsLength = length(dontEnums);
+
+  if (!isObject(obj)) throw new TypeError('Keys called on non-object');
+
+  const result = [];
+
+  for (const prop in obj) {
+    if (has(prop, obj)) result.push(prop);
+  }
+
+  if (hasDontEnumBug) {
+    for (let i = 0; i < dontEnumsLength; i++) {
+      const prop = nth(i, dontEnums);
+      if (has(prop, obj)) {
+        result.push(prop);
+      }
+    }
+  }
+
+  return result;
+};
+
+export default Object.keys || _keys;
