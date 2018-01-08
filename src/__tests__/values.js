5d6fab1ab7f2cc8670cf00f1ddaf5eb93c6a89b1
Adam Bergman <adam@fransvilhelm.com>
2018-01-08 21:38:45 +0100

    feat(values): Add module values
    Return values from an object

---
+++ src/__tests__/values.js
@@ -0,0 +1,12 @@
+import values, { _values } from '../values';
+
+test('Core.values', () => {
+  expect(values({ a: 1, b: 2, c: 3 })).toEqual([1, 2, 3]);
+
+  global.Object.values = undefined;
+  expect(values({ a: 1, b: 2, c: 3 })).toEqual([1, 2, 3]);
+});
+
+test('Internal._values', () => {
+  expect(_values({ a: 1, b: 2, c: 3 })).toEqual([1, 2, 3]);
+});
