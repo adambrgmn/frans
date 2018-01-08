23e084aec22fac292020d49e4cfa091275b12800
Adam Bergman <adam@fransvilhelm.com>
2018-01-08 16:40:34 +0100

    feat(pluck): Add module pluck

---
+++ src/__tests__/pluck.js
@@ -0,0 +1,9 @@
+import pluck from '../pluck';
+
+test('Core.pluck', () => {
+  expect(pluck('a', [{ a: 1 }, { a: 2 }, { b: 3 }])).toEqual([1, 2, undefined]);
+  expect(pluck('val', { a: { val: 1 }, b: { val: 2 } })).toEqual({
+    a: 1,
+    b: 2,
+  });
+});
