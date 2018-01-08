aac6fc4996e312baab830918a7430a31232c7b40
Adam Bergman <adam@fransvilhelm.com>
2018-01-08 13:42:15 +0100

    feat(adjust): Add module adjust
    Applies a function onto given index of an array

---
+++ src/__tests__/adjust.js
@@ -0,0 +1,12 @@
+import adjust from '../adjust';
+
+test('Core.adjust', () => {
+  const add = x => y => x + y;
+  const list = [0, 1, 2];
+
+  expect(adjust(add(10), 1, list)).toEqual([0, 11, 2]);
+  expect(adjust(add(10), 1, list)).not.toBe(list);
+
+  expect(adjust(add(10), -1, list)).toEqual([0, 1, 12]);
+  expect(adjust(add(10), 10, list)).toBe(list);
+});
