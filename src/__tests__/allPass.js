1f511fee2da2481c24ffcaac12929cd7e961b8c1
Adam Bergman <adam@fransvilhelm.com>
2018-01-08 01:36:22 +0100

    fix(allPass/anyPass): Fix wrong args being applied

--- src/__tests__/allPass.js
+++ src/__tests__/allPass.js
@@ -4,4 +4,6 @@ test('Core.allPass', () => {
   expect(allPass([() => true, () => true], 'foo')).toBeTruthy();
   expect(allPass([() => true, () => false], 'foo')).toBeFalsy();
   expect(allPass([() => false, () => false], 'foo')).toBeFalsy();
+  expect(allPass([x => x > 0, x => x < 10], 5)).toBeTruthy();
+  expect(allPass([(x, y) => x === y], 'foo', 'foo')).toBeTruthy();
 });
