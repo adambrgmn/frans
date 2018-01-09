3ba5b1c83001e640f3525a2a06a907e660a65076
Adam Bergman <adam@fransvilhelm.com>
2018-01-09 16:51:34 +0100

    feat(subtract): Add module subtract

---
+++ src/__tests__/subtract.js
@@ -0,0 +1,7 @@
+import { subtract } from '../';
+
+describe('Core.subtract', () => {
+  test('Subtracts the second argument from the first', () => {
+    expect(subtract(10, 8)).toBe(2);
+  });
+});
