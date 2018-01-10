3999a0071416db1028f0fb39e5998eb38cfd5c84
Adam Bergman <adam@fransvilhelm.com>
2018-01-10 15:13:33 +0100

    feat(groupBy): Add module groupBy

---
+++ src/groupBy.js
@@ -0,0 +1,7 @@
+import reduceBy from './reduceBy';
+import append from './append';
+
+const groupBy = (keyFn, list) =>
+  reduceBy((acc, val) => append(val, acc), [], keyFn, list);
+
+export { groupBy as default };
