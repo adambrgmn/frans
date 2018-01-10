8ba0601b18ccb59b459835b499b9f4aaf14910c6
Adam Bergman <adam@fransvilhelm.com>
2018-01-10 15:06:22 +0100

    feat(reduceBy): Add module reduceBy

---
+++ src/__tests__/reduceBy.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-nested-ternary */
+import reduceBy from '../reduceBy';
+
+describe('Core.reduceBy', () => {
+  test('splits the list into groups according to the grouping function', () => {
+    const grade = score =>
+      score < 65
+        ? 'F'
+        : score < 70 ? 'D' : score < 80 ? 'C' : score < 90 ? 'B' : 'A';
+
+    const students = [
+      { name: 'Abby', score: 84 },
+      { name: 'Brad', score: 73 },
+      { name: 'Chris', score: 89 },
+      { name: 'Dianne', score: 99 },
+      { name: 'Eddy', score: 58 },
+      { name: 'Fred', score: 67 },
+      { name: 'Gillian', score: 91 },
+      { name: 'Hannah', score: 78 },
+      { name: 'Irene', score: 85 },
+      { name: 'Jack', score: 69 },
+    ];
+
+    const byGrade = student => grade(student.score || 0);
+    const collectNames = (acc, student) => acc.concat(student.name);
+
+    expect(reduceBy(collectNames, [], byGrade, students)).toEqual({
+      A: ['Dianne', 'Gillian'],
+      B: ['Abby', 'Chris', 'Irene'],
+      C: ['Brad', 'Hannah'],
+      D: ['Fred', 'Jack'],
+      F: ['Eddy'],
+    });
+  });
+
+  test('returns an empty object if given an empty array', () => {
+    const byType = obj => obj.type;
+    const sumValues = (acc, obj) => acc + obj.val;
+
+    expect(reduceBy(sumValues, 0, byType, [])).toEqual({});
+  });
+});
---
+++ src/reduceBy.js
@@ -0,0 +1,16 @@
+import reduce from './reduce';
+import has from './has';
+import assoc from './assoc';
+
+const reduceBy = (valFn, valAcc, keyFn, list) =>
+  reduce(
+    (acc, item) => {
+      const key = keyFn(item);
+      const val = valFn(has(key, acc) ? acc[key] : valAcc, item);
+      return assoc(key, val, acc);
+    },
+    {},
+    list,
+  );
+
+export { reduceBy as default };
