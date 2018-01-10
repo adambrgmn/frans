dc02100ad32cae211353ed1e4270ad988abe3100
Adam Bergman <adam@fransvilhelm.com>
2018-01-10 14:19:23 +0100

    feat(mean): Add module mean
    Get the mean of a list of numbers

---
+++ src/__tests__/mean.js
@@ -0,0 +1,22 @@
+import mean from '../mean';
+
+describe('Core.mean', () => {
+  test('return mean from a list of numbers', () => {
+    expect(mean([2])).toBe(2);
+    expect(mean([2, 7])).toBe(4.5);
+    expect(mean([2, 7, 9])).toBe(6);
+    expect(mean([2, 7, 9, 10])).toBe(7);
+  });
+
+  test('returns NaN for empty list', () => {
+    expect(mean([])).toBe(NaN);
+  });
+
+  test('handles array-like object', () => {
+    function getArgs() {
+      return arguments; // eslint-disable-line prefer-rest-params
+    }
+
+    expect(mean(getArgs(1, 2, 3))).toBe(2);
+  });
+});
---
+++ src/mean.js
@@ -0,0 +1,11 @@
+import sum from './sum';
+import divide from './divide';
+import length from './length';
+
+const mean = list => {
+  const l = length(list);
+  const s = sum(list);
+  return divide(s, l);
+};
+
+export { mean as default };
