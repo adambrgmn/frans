d84c10484734726433822d1ac4925a823b597fcb
Adam Bergman <adam@fransvilhelm.com>
2018-01-10 16:04:56 +0100

    feat(dissoc): Add module dissoc
    Remove a prop from an object

---
+++ src/__tests__/dissoc.js
@@ -0,0 +1,17 @@
+import dissoc from '../dissoc';
+
+describe('Core.dissoc', () => {
+  test('copies an object omitting the specified property', () => {
+    expect(dissoc('b', { a: 1, b: 2, c: 3 })).toEqual({ a: 1, c: 3 });
+    expect(dissoc('d', { a: 1, b: 2, c: 3 })).toEqual({ a: 1, b: 2, c: 3 });
+  });
+
+  test('coerces non-string types', () => {
+    expect(dissoc(42, { a: 1, b: 2, 42: 3 })).toEqual({ a: 1, b: 2 });
+    expect(dissoc(null, { a: 1, b: 2, null: 3 })).toEqual({ a: 1, b: 2 });
+    expect(dissoc(undefined, { a: 1, b: 2, undefined: 3 })).toEqual({
+      a: 1,
+      b: 2,
+    });
+  });
+});
