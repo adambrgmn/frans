4974d144510b21c5a604c57b9d29f7e25264e656
Adam Bergman <adam@fransvilhelm.com>
2018-01-11 10:17:45 +0100

    feat(any): Run a list through a predicate function and return true if anyone passes

---
+++ src/any.js
@@ -0,0 +1,18 @@
+import length from './length';
+
+const any = (fn, list) => {
+  const l = length(list);
+  let i = 0;
+  let ret = false;
+
+  while (!ret) {
+    if (i >= l) break;
+
+    ret = fn(list[i]);
+    i += 1;
+  }
+
+  return ret;
+};
+
+export { any as default };
