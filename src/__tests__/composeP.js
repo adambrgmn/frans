9fba621e9c3da2b6d8f7540d2ef999f17d88682f
Adam Bergman <adam@fransvilhelm.com>
2018-01-11 16:22:30 +0100

    feat(composeP): Add module composeP
    Composes a list of promises together

---
+++ src/__tests__/composeP.js
@@ -0,0 +1,12 @@
+import composeP from '../composeP';
+
+describe('Core.composeP', () => {
+  const splitAsync = string => Promise.resolve(string.split(''));
+  const reverseAsync = arr => Promise.resolve(arr.reverse());
+  const joinAsync = arr => Promise.resolve(arr.join(''));
+
+  test('creates an async compose chain', async () => {
+    const reverseStringAsync = composeP(joinAsync, reverseAsync, splitAsync);
+    expect(await reverseStringAsync('hello')).toBe('olleh');
+  });
+});
