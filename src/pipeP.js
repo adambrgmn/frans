259b056f0fcb7b148dc8457c1204718dcf3f161b
Adam Bergman <adam@fransvilhelm.com>
2018-01-11 20:48:45 +0100

    fix(pipeP): Remove async keyword
    Unecessary keyword that was forgotten after experiment

--- src/pipeP.js
+++ src/pipeP.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 import reduce from './reduce';
 
-const _pipeP = (f, g) => async (...x) => f(...x).then(g);
+const _pipeP = (f, g) => (...x) => f(...x).then(g);
 const pipeP = (init, ...fns) => reduce(_pipeP, init, fns);
 
 export { pipeP as default };
