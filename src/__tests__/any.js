1544e65cc389e299d2d732ce4168b164f775fa8a
Adam Bergman <adam@fransvilhelm.com>
2018-01-11 10:17:45 +0100

    feat(any): Run a list through a predicate function and return true if anyone passes

---
+++ src/__tests__/any.js
@@ -0,0 +1,44 @@
+import any from '../any';
+
+describe('Core.any', () => {
+  const odd = n => n % 2 === 1;
+  const T = () => true;
+
+  test('returns true if any element satisfies the predicate', () => {
+    expect(any(odd, [2, 4, 6, 8, 10, 11, 12])).toBeTruthy();
+  });
+
+  test('returns false if all elements fails to satisfy the predicate', () => {
+    expect(any(odd, [2, 4, 6, 8, 10, 12])).toBeFalsy();
+  });
+
+  test('works with more complex objects', () => {
+    const alliterative = p => p.first.charAt(0) === p.last.charAt(0);
+    const people = [
+      { first: 'Paul', last: 'Grenier' },
+      { first: 'Mike', last: 'Hurley' },
+      { first: 'Will', last: 'Klein' },
+    ];
+
+    expect(any(alliterative, people)).toBeFalsy();
+
+    people.push({ first: 'Scott', last: 'Sauyet' });
+    expect(any(alliterative, people)).toBeTruthy();
+  });
+
+  test('can use a configurable function', () => {
+    const atLeast = age => p => p.age >= age;
+    const teens = [
+      { name: 'Alice', age: 14 },
+      { name: 'Betty', age: 18 },
+      { name: 'Cindy', age: 17 },
+    ];
+
+    expect(any(atLeast(16), teens)).toBeTruthy();
+    expect(any(atLeast(21), teens)).toBeFalsy();
+  });
+
+  test('returns false for an empty list', () => {
+    expect(any(T, [])).toBeFalsy();
+  });
+});
