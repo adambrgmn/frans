0ba4e9a1bc52ca02845965be0ceb657eeeab59a4
Adam Bergman <adam@fransvilhelm.com>
2018-01-11 10:16:46 +0100

    feat(all): Add module all
    Run a list through a predicate function and return true if all passes

---
+++ src/all.js
@@ -0,0 +1,18 @@
+import length from './length';
+
+const all = (fn, list) => {
+  const l = length(list);
+  let i = 0;
+  let ret = true;
+
+  while (ret) {
+    if (i >= l) break;
+
+    ret = fn(list[i]);
+    i += 1;
+  }
+
+  return ret;
+};
+
+export { all as default };
