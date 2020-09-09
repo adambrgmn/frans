4b2bb3c28ecc95c661dbf2c9d5fd96de4d23fb83
Adam Bergman <adam@fransvilhelm.com>
2020-09-09 07:55:14 +0200

    Fix failing test

--- src/__tests__/memoizeWith.js
+++ src/__tests__/memoizeWith.js
@@ -4,7 +4,7 @@ describe('Core.memoizeWith', () => {
   const memo = memoizeWith((...args) => args.join(''));
 
   test('Creates a memoization function', () => {
-    const greet = jest.fn(name => `Hello, ${name}!`);
+    const greet = jest.fn((name) => `Hello, ${name}!`);
     const greetMemoized = memo(greet);
 
     greetMemoized();
@@ -13,7 +13,8 @@ describe('Core.memoizeWith', () => {
     expect(greet).toHaveBeenCalledTimes(1);
   });
 
-  test('Saves result the first time and returns it');
-  const getArr = memo((a, b) => [a, b]);
-  expect(getArr(1, 2)).toBe(getArr(1, 2));
+  test('Saves result the first time and returns it', () => {
+    const getArr = memo((a, b) => [a, b]);
+    expect(getArr(1, 2)).toBe(getArr(1, 2));
+  });
 });
