867b8e3b1405a2f90ea287a58838184c0c7a6043
Adam Bergman <adam@fransvilhelm.com>
2020-09-09 10:38:07 +0200

    Fix ts warning

--- src/__tests__/clamp.ts
+++ src/__tests__/clamp.ts
@@ -2,7 +2,7 @@ import { clamp } from '../clamp';
 
 describe('Core.clamp', () => {
   test('restrict number by min and max values', () => {
-    const min0Max10 = (n) => clamp(0, 10, n);
+    const min0Max10 = (n: number) => clamp(0, 10, n);
     expect(min0Max10(0)).toBe(0);
     expect(min0Max10(0)).toBe(0);
     expect(min0Max10(-1)).toBe(0);
