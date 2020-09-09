f8934f15f002edadd1a9496e2a7ac2d5758aef60
Adam Bergman <adam@fransvilhelm.com>
2020-09-09 10:29:59 +0200

    Move comparator to ts

---
+++ .changeset/tricky-kings-complain.md
@@ -0,0 +1,5 @@
+---
+'frans': minor
+---
+
+Move comparator to ts
--- src/__tests__/comparator.ts
+++ src/__tests__/comparator.ts
@@ -2,8 +2,8 @@ import { comparator } from '../comparator';
 
 describe('Core.comparator', () => {
   test('builds a comparator function for sorting out of a simple predicate that reports whether the first param is smaller', () => {
-    const compOne = comparator((a, b) => a < b);
-    const compTwo = comparator((a, b) => a > b);
+    const compOne = comparator((a: number, b: number) => a < b);
+    const compTwo = comparator((a: number, b: number) => a > b);
 
     expect([3, 1, 8, 1, 2, 5].sort(compOne)).toEqual([1, 1, 2, 3, 5, 8]);
     expect([3, 1, 8, 1, 2, 5].sort(compTwo)).toEqual([8, 5, 3, 2, 1, 1]);
